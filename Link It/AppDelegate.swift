0e12d5083c212b2503219c598d783144fa10a2ff
Royce <royce.com@gmail.com>
2021-04-02 08:04:45 +0800

    Initial Commit

---
+++ Link It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Link It
+//
+//  Created by Royce Remulla on 4/2/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Link It/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Link It
+//
+//  Created by Royce Remulla on 4/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
