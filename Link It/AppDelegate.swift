d6d1956269d32d9df4dd79888c821ea9116e5c6a
Royce <royce.com@gmail.com>
2021-04-02 17:18:08 +0800

    Concluded

--- Link It/AppDelegate.swift
+++ Link It/AppDelegate.swift
@@ -9,18 +9,50 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
-
+    var theStatusItem: NSStatusItem?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        theStatusItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        if let item = theStatusItem {
+            if let menuButton = item.button {
+                menuButton.image = NSImage(named: "link-32")
+                
+                let menu = NSMenu()
+                menu.addItem(NSMenuItem(title: "Link It", action: #selector(AppDelegate.linkIt), keyEquivalent: ""))
+                menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit), keyEquivalent: ""))
+                
+                theStatusItem?.menu = menu
+            }
+        }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return false
+        // return true // For testing in Xcode, we want to simple quit the app on window close
     }
-
-
+    
+    @objc
+    func linkIt() {
+        print("Clicked")
+        printPasteBoard()
+    }
+    
+    @objc
+    func quit() {
+        NSApplication.shared.terminate(self)
+    }
+    
+    @objc
+    func printPasteBoard() {
+        if let items = NSPasteboard.general.pasteboardItems {
+            for item in items {
+                for type in item.types {
+                    print(type)
+                }
+            }
+        }
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,22 @@
+# Link It
+
+Followed through from uDemy The Complete MacOS Developer Course - Apps for the Desktop!
+
+[Course](https://www.udemy.com/course/macos-programming-for-ios-developers-mac-apps-os-x-cocoa/learn/lecture/5935202#overview)
+
+## Notes
+
+Icon adapts to light/dark theme automatically by setting the image render as Template in the Assets view.
+Set image size to 32 for images that appear in the menu bar.
+
+## Features
+Lives in the System Status
+No App Icon
+
+## Pending
+
+Did not implement fully because I don't have use for it yet.
+
+## References
+
+https://developer.apple.com/documentation/uikit/uiimage/providing_images_for_different_appearances
